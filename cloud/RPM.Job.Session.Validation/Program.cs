f3c9792487d509edd3ac24e3d6f2e8af67d97c87
Ashwin Kannoth <ashwin.kannoth@tesplabs.com>
2025-07-18 22:11:50 +0530

    Add Session validation Job

---
+++ cloud/RPM.Job.Session.Validation/Program.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace SessionValidationJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    internal class Program
+    {
+        static string CONN_STRING = string.Empty;
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables() // Allows overriding via Azure App Settings
+            .Build();
+            if (config == null)
+            {
+                Console.WriteLine("Configuration is null.");
+                return;
+            }
+            // Access a specific config value
+            string? connStr = config["RPM:ConnectionString"];
+            Console.WriteLine($"RPM Connection String: {connStr}");
+            if (connStr == null)
+            {
+                Console.WriteLine("Connection string is null in appsettings.json.");
+                return;
+            }
+            CONN_STRING = connStr;
+            Console.WriteLine("Starting Session validation WebJob...");
+
+            if (string.IsNullOrWhiteSpace(CONN_STRING))
+            {
+                Console.WriteLine("Connection string not found in environment variables.");
+                return;
+            }
+
+            try
+            {
+                IsTokenExpired();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.WriteLine("WebJob finished.");
+        }
+        public static void IsTokenExpired()
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                RequireSignedTokens = false,
+                ValidateIssuerSigningKey = false,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                SignatureValidator = (token, parameters) => { return new JwtSecurityToken(token); }
+
+            };
+            List<string> Tokens = GetAllLoginSessions(CONN_STRING);
+            foreach (string jwtToken in Tokens)
+            {
+
+                try
+                {
+                    ClaimsPrincipal claimsPrincipal = tokenHandler.ValidateToken(jwtToken, tokenValidationParameters, out SecurityToken validatedToken);
+                }
+                catch (SecurityTokenExpiredException)
+                {
+                    UpdInvalidSessionZero(jwtToken, CONN_STRING);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Not able to validate token");
+                }
+            }
+
+        }
+
+        public static List<string> GetAllLoginSessions(string ConnectionString)
+        {
+            try
+            {
+                List<string> Tokens = new List<string>();
+
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand command = new SqlCommand("usp_GetAllLoginSessions", con);
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter returnParameter = command.Parameters.Add("RetVal", SqlDbType.Int);
+                    returnParameter.Direction = ParameterDirection.ReturnValue;
+                    con.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string? jwtToken = Convert.ToString(reader["JwtToken"]);
+                        Tokens.Add(jwtToken);
+                    }
+
+                    con.Close();
+                }
+                return Tokens;
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+        public static void UpdInvalidSessionZero(string jwtToken, string ConnectionString)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand command = new SqlCommand("usp_UpdInvalidSessionzero", con);
+                    command.Parameters.AddWithValue("@JwtToken", jwtToken);
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlParameter returnParameter = command.Parameters.Add("RetVal", SqlDbType.Int);
+                    returnParameter.Direction = ParameterDirection.ReturnValue;
+                    con.Open();
+                    command.ExecuteReader();
+                    con.Close();
+                }
+
+            }
+            catch (Exception ex) { throw ex; }
+
+        }
+
+    }
+}
